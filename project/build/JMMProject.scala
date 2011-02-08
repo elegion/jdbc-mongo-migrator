2b69ff01f4520c457419e4acabab4de4bf2d3c46
Yuri Buyanov <yuri.buyanov@e-legion.com>
2011-02-08 18:52:35 +0300

    added proguard plugin to create standalone jar

--- project/build/JMMProject.scala
+++ project/build/JMMProject.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class JMMProject(info: ProjectInfo) extends DefaultProject(info)
+class JMMProject(info: ProjectInfo) extends DefaultProject(info) with ProguardProject
 {
   val lift_json = "net.liftweb" %% "lift-json" % "2.2"
   val casbah = "com.mongodb.casbah" %% "casbah" % "2.0.2"
@@ -9,4 +9,19 @@ class JMMProject(info: ProjectInfo) extends DefaultProject(info)
   val configgy = "net.lag" % "configgy" % "2.0.0" intransitive()
   lazy val hi = task { println("Hello World"); None }
 
+//program entry point
+  override def mainClass: Option[String] = Some("jm.migrator.Launcher")
+
+
+    //proguard
+  override def proguardOptions = List(
+    "-keepclasseswithmembers public class * { public static void main(java.lang.String[]); }",
+    "-dontoptimize",
+    "-dontobfuscate",
+    "-dontshrink",
+    proguardKeepLimitedSerializability,
+    proguardKeepAllScala
+    //"-keep interface scala.ScalaObject"
+  )
+  override def proguardInJars = Path.fromFile(scalaLibraryJar) +++ super.proguardInJars
 }
