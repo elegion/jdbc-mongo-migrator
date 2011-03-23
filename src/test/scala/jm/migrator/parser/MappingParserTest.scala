974324be7fcfd737ec8537f5b9aec5d997a1816f
Yuri Buyanov <yuri.buyanov@e-legion.com>
2011-03-23 14:03:25 +0300

    MappingParserTest with mapping object check

--- src/test/scala/jm/migrator/parser/MappingParserTest.scala
+++ src/test/scala/jm/migrator/parser/MappingParserTest.scala
@@ -25,6 +25,26 @@ class MappingParserTest extends Spec with MustMatchers {
       result.head.mapping.fields("_id") must equal (MongoId("u.id", "users"))
       result.head.mapping.fields("stringId") must equal (StringMongoId("u.id", "users"))
       result.head.mapping.fields("oldId") must equal (SimpleValue("u.id"))
+      result.head.mapping.fields("username") must equal (SimpleValue("u.username"))
+      result.head.mapping.fields("counters.rating") must equal (ToInt("p.rating"))
+      result.head.mapping.fields("counters.following") must equal (Count("following_following", "user_id = ${oldId}"))
+      result.head.mapping.fields("counters.followers") must equal (Count("following_following", "victim_id = ${oldId}"))
+      result.head.mapping.fields("groups") must equal ( Array (
+        "groups AS g LEFT JOIN members AS m ON g.id = m.group_id",
+        SimpleValue("LOWER(g.slug)"),
+        "banned = 0 AND m.user_id = ${oldId}"))
+      result.head.mapping.fields("groupPostCount") must equal ( CountMap (
+        "posts AS p LEFT JOIN groups AS g ON p.group_id = g.id",
+        "g.slug IS NOT NULL AND p.user_id = ${oldId}",
+        "LOWER(g.slug)"
+      ))
+      result.head.mapping.fields("invites") must equal ( Array (
+        "group_invites AS i LEFT JOIN groups AS g ON i.group_id = g.id",
+        Fields(Map("group" -> SimpleValue("LOWER(g.slug)"), "invitedBy" -> StringMongoId("i.invited_by_id", "users"))),
+        "i.user_id = ${oldId}"
+      ))
+
+
     }
 
   }
