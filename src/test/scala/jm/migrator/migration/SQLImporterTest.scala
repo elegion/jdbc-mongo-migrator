4227a168e11edd7a8efccdf90bc23ed479d88a6e
Yuri Buyanov <yuri.buyanov@e-legion.com>
2011-04-05 18:00:02 +0400

    insert test draft

--- src/test/scala/jm/migrator/migration/SQLImporterTest.scala
+++ src/test/scala/jm/migrator/migration/SQLImporterTest.scala
@@ -6,7 +6,11 @@ import java.sql.{DriverManager, Connection}
 import net.lag.configgy.Configgy
 import org.scalatest.matchers.MustMatchers
 import jm.migrator.db.DBUtil._
-import jm.migrator.db.MongoInsertBackend
+import jm.migrator.db.InsertBackend
+import com.mongodb.DBObject
+
+import com.mongodb.casbah.Imports._
+
 ;
 
 
@@ -14,6 +18,23 @@ import jm.migrator.db.MongoInsertBackend
  * Author: Yuri Buyanov
  * Date: 3/23/11 2:52 PM
  */
+trait TestInsertBackend extends InsertBackend {
+  import scala.collection.mutable.Map
+  val objMap: Map[String, List[DBObject]] = Map.empty
+
+
+  def doInsert = doInsertInternal
+
+  def doInsertInternal(objects: Seq[DBObject], collectionName: String) = {
+    objMap += collectionName ->
+                 (objMap.get(collectionName).getOrElse(List.empty) ++ objects)
+  }
+
+  def getInserts(collectionName: String) = {
+    objMap get collectionName getOrElse List.empty
+  }
+}
+
 class SQLImporterTest extends Spec with MustMatchers {
   val config = getClass.getResource("/test_config.conf").getFile
   Configgy.configure(config)
@@ -29,9 +50,16 @@ class SQLImporterTest extends Spec with MustMatchers {
 
       val parser = new MappingParser
       val mapping = parser.parseFile(getClass.getResource("/test_mapping.json").getFile)
-      val importer = new SQLImporter(mapping) with MongoInsertBackend
+      val importer = new SQLImporter(mapping) with TestInsertBackend
       it ("Should fetch data") {
         val fetchResult = importer.fetch
+        val userInserts = importer.getInserts("users")
+        userInserts must have size(5)
+        val user0 = userInserts(0)
+        user0.getAs[String]("username") must equal (Some("user0"))
+        user0.expand[Int]("counters.rating") must equal (Some(0))
+
+        //TODO: further tests
       }
     }
 
